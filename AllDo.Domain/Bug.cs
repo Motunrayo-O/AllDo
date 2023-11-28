b14d3b0294d466917e147f9ebcb48141b433f5cf
motunrayo-o <motunrayo.ogunyinka@gmail.com>
2023-11-28 10:08:21 +0000

    update config files

--- AllDo.Domain/Bug.cs
+++ AllDo.Domain/Bug.cs
@@ -7,4 +7,5 @@ public enum Severity
     Minor,
     Irritating
 }
-public record Bug(string Title, string Description, Severity Severity, string AffectedVersion, int AffectedUsers, User CreatedBy, User? AssignedTo, IEnumerable<Byte[]> Images) : TodoTask(Title, DateTimeOffset.MinValue, CreatedBy);
+public record Bug(string Title, string Description, Severity Severity, string AffectedVersion, 
+int AffectedUsers, User CreatedBy, User? AssignedTo, IEnumerable<Byte[]> Images) : TodoTask(Title, DateTimeOffset.MinValue, CreatedBy);
