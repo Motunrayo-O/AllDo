7750281010024250aab766bc81e3175cb1c3e35c
motunrayo-o <motunrayo.ogunyinka@gmail.com>
2023-12-30 12:32:22 +0000

    Add DBContext

---
+++ AllDo.Infrastructure/Data/AllDoDbContext.cs
@@ -0,0 +1,18 @@
+using AllDo.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AllDo.Infrastructure.Data;
+
+public class AllDoDbContext : DbContext
+{
+    public DbSet<TodoTask> TodoTasks { get; set; }
+    public DbSet<Bug> Bugs { get; set; }
+    public DbSet<Feature> Features { get; set; }
+    public DbSet<Image> Images { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=AllDo.db");
+    }
+}
